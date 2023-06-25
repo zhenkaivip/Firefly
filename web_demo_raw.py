d8328ea90f594ab24987b6037012f16401c7da4f
zhenkaivip <zhenkaivip@163.com>
2023-06-25 12:35:48 +0800

    raw baichuan

---
+++ web_demo_raw.py
@@ -0,0 +1,96 @@
+import gradio as gr
+
+from transformers.utils.versions import require_version
+
+require_version("gradio>=3.30.0", "To fix: pip install gradio>=3.30.0")
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+import torch
+from transformers import TextIteratorStreamer
+from threading import Thread
+
+device = 'cuda'
+model_name = 'baichuan-inc/baichuan-7B'
+repetition_penalty = 1.0
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    trust_remote_code=True,
+    low_cpu_mem_usage=True,
+    torch_dtype=torch.float16,
+    device_map='auto'
+)
+model.eval()
+model = model.to(device)
+
+
+def predict(query, chatbot, max_new_tokens, top_p, temperature, history):
+    print(query)
+    chatbot.append((query, ""))
+    model_input_ids = tokenizer(query, return_tensors="pt").input_ids
+    model_input_ids = model_input_ids.to(device)
+
+    streamer = TextIteratorStreamer(tokenizer, timeout=60.0, skip_prompt=True, skip_special_tokens=True)
+
+    gen_kwargs = {}
+    gen_kwargs["input_ids"] = model_input_ids
+    gen_kwargs["max_new_tokens"] = max_new_tokens
+    gen_kwargs["do_sample"] = True
+    gen_kwargs["top_p"] = top_p
+    gen_kwargs["temperature"] = temperature
+    gen_kwargs["repetition_penalty"] = repetition_penalty
+    gen_kwargs["eos_token_id"] = tokenizer.eos_token_id
+    gen_kwargs["streamer"] = streamer
+
+    thread = Thread(target=model.generate, kwargs=gen_kwargs)
+    thread.start()
+
+    response = ""
+    for new_text in streamer:
+        response += new_text
+        new_history = history + [(query, response)]
+        chatbot[-1] = (query, response)
+        yield chatbot, new_history
+
+
+def reset_user_input():
+    return gr.update(value="")
+
+
+def reset_state():
+    return [], []
+
+
+with gr.Blocks() as demo:
+    gr.HTML("""
+    <h1 align="center">
+        baichuan-inc/baichuan-7B
+    </h1>
+    """)
+
+    chatbot = gr.Chatbot()
+
+    with gr.Row():
+        with gr.Column(scale=4):
+            with gr.Column(scale=12):
+                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10).style(container=False)
+            with gr.Column(min_width=32, scale=1):
+                submitBtn = gr.Button("Submit", variant="primary")
+
+        with gr.Column(scale=1):
+            emptyBtn = gr.Button("Clear History")
+            max_new_tokens = gr.Slider(0, 1024, value=500, step=1.0, label="Maximum length", interactive=True)
+            top_p = gr.Slider(0, 1, value=0.9, step=0.01, label="Top P", interactive=True)
+            temperature = gr.Slider(0, 1.5, value=1.00, step=0.01, label="Temperature",
+                                    interactive=True)
+
+    history = gr.State([])
+
+    submitBtn.click(predict, [user_input, chatbot, max_new_tokens, top_p, temperature, history], [chatbot, history],
+                    show_progress=True)
+    submitBtn.click(reset_user_input, [], [user_input])
+
+    emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
+
+demo.queue().launch(server_name="0.0.0.0", share=True, inbrowser=False, server_port=10002)
